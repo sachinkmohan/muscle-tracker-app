6e1f338c11bc8a0b514a7d0396da273219856c25
Sachin KM <hiscan99@gmail.com>
2024-05-19 20:40:36 +0200

    add SVG component

---
+++ src/components/SVGComponent.tsx
@@ -0,0 +1,11 @@
+import fileSvg from "../assets/front_muscles.svg";
+
+function SVGComponent() {
+  return (
+    <div>
+      <img src={fileSvg} alt="SVG File" />
+    </div>
+  );
+}
+
+export default SVGComponent;
