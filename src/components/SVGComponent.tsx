84e2d07c4a7a83d178c8057da5ecf2fb502efaa2
Sachin KM <hiscan99@gmail.com>
2024-06-10 23:09:52 +0200

    wip click on the SVG image

--- src/components/SVGComponent.tsx
+++ src/components/SVGComponent.tsx
@@ -1,9 +1,34 @@
-import fileSvg from "../assets/front_muscles.svg";
+import React, { useState } from "react";
+import { ReactComponent as FileSvg } from "../assets/front_muscles.svg";
 
+const MuscleDiagram = () => {
+  const [selectedMuscle, setSelectedMuscle] = useState<string | null>(null);
+
+  const handleMuscleClick = (muscleName: string) => {
+    setSelectedMuscle(muscleName);
+    console.log(`${muscleName} clicked!`);
+  };
+};
 function SVGComponent() {
   return (
-    <div className="flex justify-center">
-      <img src={fileSvg} alt="SVG File" className="h-96 place-content-center" />
+    <div>
+      <div className="flex justify-center">
+        <img
+          src={FileSvg}
+          alt="SVG File"
+          className="h-96 place-content-center"
+        />
+      </div>
+      <FileSvg
+        onClick={(e: any) => {
+          if (e.target.id === "abs") {
+            handleMuscleClick("Abs");
+          } else if (e.target.id === "chest") {
+            handleMuscleClick("Chest");
+          }
+        }}
+      />
+      {selectedMuscle && <p>{selectedMuscle} selected</p>}
     </div>
   );
 }
